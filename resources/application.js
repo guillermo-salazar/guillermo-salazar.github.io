b611bce471663a90d8c46c30730e7675e18fb652
guillermo-salazar <guillermos_2007@yahoo.com>
2015-05-18 13:32:30 +0000

    initial setup

---
+++ resources/application.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    reslide();
+});
+
+$("#about").css("color", "gray").css("max-width","50%");
+$("body").css("background-color","white");
+$("#aboutMe")
+  .show()
+  .addClass("selectable")
+  .click(function (e){
+  alert("You clicked something!");
+});
+$(".selectable").click(function(e){
+  alert("You clicked something!");
+});
+
+// $("#style").change(function(){
+  
+//   var selectedStyle= $(this).val();
+//   console.log(selectedStyle);
+//   switch(selectedStyle){
+//     case 'blue': $("body").css('background-color','blue'); break;
+//     case 'green': $("body").css('background-color','green');break;
+//     case 'gray': $("body").css('background-color','gray');break;
+//   }
+// });
+
+// function reslide(){
+//   $('.slide-right').delay(1000).animate({width:'0'}, 600).delay(8000).animate({width:'100%'}, 200, function(){
+// setTimeout(reslide, 3000);
+// }); 
+// }
