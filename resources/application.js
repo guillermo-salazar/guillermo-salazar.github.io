44b4749c9786ecd43f31e6d6df17210f4524ef6e
guillermo_salazar <guillermos_2007@yahoo.com>
2015-05-26 02:44:01 +0000

    update about me template

--- resources/application.js
+++ resources/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    reslide();
+//    reslide();
 });
 
 $("#about").css("color", "gray").css("max-width","50%");
-$("body").css("background-color","white");
+//$("body").css("background-color","white");
 $("#aboutMe")
   .show()
   .addClass("selectable")
@@ -14,16 +14,16 @@ $(".selectable").click(function(e){
   alert("You clicked something!");
 });
 
-// $("#style").change(function(){
+$("#style").change(function(){
   
-//   var selectedStyle= $(this).val();
-//   console.log(selectedStyle);
-//   switch(selectedStyle){
-//     case 'blue': $("body").css('background-color','blue'); break;
-//     case 'green': $("body").css('background-color','green');break;
-//     case 'gray': $("body").css('background-color','gray');break;
-//   }
-// });
+  var selectedStyle= $(this).val();
+  console.log(selectedStyle);
+  switch(selectedStyle){
+    case 'blue': $("body").css('background-color','blue'); break;
+    case 'green': $("body").css('background-color','green');break;
+    case 'gray': $("body").css('background-color','gray');break;
+  }
+});
 
 // function reslide(){
 //   $('.slide-right').delay(1000).animate({width:'0'}, 600).delay(8000).animate({width:'100%'}, 200, function(){
